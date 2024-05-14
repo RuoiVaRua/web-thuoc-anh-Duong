f10735350ac60d03740942c2fdea725bf80544be
hung-vp <hung.vu@giaminhmedia.vn>
2024-05-14 16:50:05 +0700

    small update

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,7 +1,7 @@
 const currentPage = JSON.parse(localStorage.getItem('current-page'));
 
-function appendContent(url, targetSelector) {
-    fetch(url)
+  async function appendContent(url, targetSelector) {
+    await fetch(url)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -27,15 +27,28 @@ function appendContent(url, targetSelector) {
     }
   }
   
-  appendContent('./components/topbar.html', '.main-wrapper');
-  appendContent('./components/header.html', '.main-wrapper');
-  appendContent('./components/footer.html', '.main-wrapper');
-
-  if (!currentPage || currentPage === 'home') {
-    appendContent('./components/home-slider.html', '.main-wrapper');
-    appendContent('./pages/home.html', '.main-wrapper');
-  }
-  else if (['about', 'contact', 'products'].includes(currentPage)) {
-    appendContent(`./pages/${currentPage}.html`, '.main-wrapper');
-  }
-  
+  (async () => {
+    await appendContent('./components/topbar.html', '.main-wrapper');
+    await appendContent('./components/header.html', '.main-wrapper');
+    
+    if (!currentPage || currentPage === 'home') {
+      // await appendContent('./pages/home.html', '.main-wrapper');
+      await appendContent('./components/home-sections/home-slider.html', '.main-wrapper');
+      await appendContent('./components/home-sections/general-introduction.html', '.main-wrapper');
+      await appendContent('./components/home-sections/hometown-specialties.html', '.main-wrapper');
+      await appendContent('./components/home-sections/products-categories.html', '.main-wrapper');
+      await appendContent('./components/home-sections/two-banners.html', '.main-wrapper');
+      await appendContent('./components/home-sections/selling-products-header.html', '.main-wrapper');
+      await appendContent('./components/home-sections/selling-products-content.html', '.main-wrapper');
+      await appendContent('./components/home-sections/customer-reviews.html', '.main-wrapper');
+      await appendContent('./components/home-sections/footer-banner.html', '.main-wrapper');
+      await appendContent('./components/home-sections/event-news.html', '.main-wrapper');
+      await appendContent('./components/home-sections/certificate-header.html', '.main-wrapper');
+      await appendContent('./components/home-sections/certificate-content.html', '.main-wrapper');
+    }
+    else if (['about', 'contact', 'products'].includes(currentPage)) {
+      await appendContent(`./pages/${currentPage}.html`, '.main-wrapper');
+    }
+    
+    await appendContent('./components/footer.html', '.main-wrapper');
+  })()
