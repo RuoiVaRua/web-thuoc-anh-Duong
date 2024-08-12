373167949911c71a3da72920bf562075ec9c8bef
hungvp <hungvp@ecoit.com.vn>
2024-08-12 11:39:31 +0700

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    publicPath:
-        process.env.NODE_ENV === "production"
-            ? "/web-thuoc-anh-Duong/"
-            : "/",
+    publicPath: "/web-thuoc-anh-Duong/"
 };
